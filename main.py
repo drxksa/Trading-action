e1115077456f293489fe4474db00a327f67af074
memeah <windowsah@ymail.com>
2023-01-24 02:31:00 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from api.oanda_api import OandaApi
+from infrastructure.instrument_collection import instrumentCollection
 
 
 if __name__ == '__main__':
      api = OandaApi()
 
+     instrumentCollection.CreateFile(api.get_account_instruments(),"./data")
 
-     data = api.get_account_summary()
-     print (data)
-
+     instrumentCollection.LoadInstruments("./data")
+     instrumentCollection.PrintInstruments()
      
